86fd27c566b494fd0656b6e9abb013388b0ebb89
danigarr <daniel.garrido-lizana@capgemini.com>
2022-05-14 16:00:33 +0200

    fix a problem whrn calling getItems()

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import { getItems } from './service/getItems';
 
 const App = () => {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState(0);
   const displayItems = () => {
-    getItems
+    getItems()
     .then(response => response.json())
-    then(data => setItems(data));
+    .then(data => setItems(data));
   };
 
   return (
     <React.Fragment>
-     <h1>Hay {items} en la base de datos de Redis</h1><hr/>
-      <button onClick= {displayItems}></button>
+     <h1>Hay: {items} en la base de datos de Redis</h1><hr/>
+      <button onClick= {displayItems}>Añadir</button>
     </React.Fragment>
   );
 };
