1610516cc4cf5f8d47ec8a9d261f9f4258879b2c
danigarr <daniel.garrido-lizana@capgemini.com>
2022-05-14 14:52:06 +0200

    created function to call to api endpoint and added logic on App.js to display de resul

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { getItems } from './service/getItems';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState();
+  const displayItems = () => {
+    getItems
+    .then(response => response.json())
+    then(data => setItems(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+     <h1>Hay {items} en la base de datos de Redis</h1><hr/>
+      <button onClick= {displayItems}></button>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
